e51cad9ee46f087b0ff3df28e1a8ee9c0e5c9064
tiffanyip <tiffanyip@ucla.edu>
2016-09-08 10:43:37 -0700

    [setup] change database username and password to process environment variable

--- .auth.js
+++ .auth.js
@@ -1,3 +1,3 @@
-googlePlacesKey = KEYgoesHERE
+googlePlacesKey = "AIzaSyBAiohPETLe5egULjfvvpqa07JW3hntgSA"
 
 //rename this file to .auth after you change the correct username and password and also remove this comment plz.
