8439248b1ee22fb554a27edcde6237d07196e2a5
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-09-09 13:52:24 -0700

    [client-feat] Adds List of search results and clickable list items to change main view on search page

--- client/components/FoodResult.js
+++ client/components/FoodResult.js
@@ -5,6 +5,7 @@ import { Image, Thumbnail, Button } from 'react-bootstrap';
 var FoodResult = (props) => (
     <div className="blurred-container-food-result result-content">
         <div className='food-result'>
+        {console.log("PROPS", props)}
             <Thumbnail src={props.foodType.value.image}>
                 <h3>{props.result.restaurant_name}</h3>
                 <div>{props.result.address}</div>
