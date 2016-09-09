8156bd0f9ce44bd36909b3f864f09975c64d650e
tiffanyip <tiffanyip@ucla.edu>
2016-09-08 18:33:23 -0700

    [server-feat] add encryption to password on signup of user

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,4 +1,5 @@
 var userModel = require('../model/user.model.js');
+var bcrypt = require('bcrypt')
 
 exports.user = {
     add: addUser,
@@ -10,11 +11,12 @@ function addUser(req, res) {
         name: req.query.name,
         password: req.query.password
     }
-    userModel.findUserById(newUser)
+    userModel.findUser(newUser)
         .then(function(user) {
             if (user) {
                 res.status(200).send("User already exists");
             } else {
+              console.log("after findUserby id ");
                 userModel.addUser(newUser)
                     .then(function(result) {
                         res.status(200).send(result)
@@ -30,23 +32,44 @@ function addUser(req, res) {
 }
 
 function checkUser(req, res) {
-    console.log("in checkuser req", req.query);
-    var findUser = {
-        name: req.query.name,
-        password: req.query.password
-    }
+  console.log("in checkuser req", req.query);
+  var lookupUser = {
+    name: req.query.name,
+    password: req.query.password
+  }
 
-    userModel.checkUser(findUser)
-        .then(function(user) {
-            if (user) {
-                console.log("successful user signin");
-                res.status(202).send(user);
-            } else {
-                res.status(200).send("Please sign up first")
-            }
-        })
-        .catch(function(err) {
-            res.status(500).end('Error inside findUserById', err)
-        })
+  userModel.findUser(lookupUser)
+  .then(function(user){
+    if(user){
+      //if user exsits, check password
+      userModel.checkUser(lookupUser)
+      .then(function(result){
+        if(result)
+          res.status(202).send('signup succesful');
+        else
+          res.status(200).send('wrong password');
+      })
+
+    }else{
+      //if user doesnt exist , return GO SIGNUP!
+      res.status(200).send("Please sign up first")
+    }
+  })
+  .catch(function(err){
+    res.status(500).end('Error inside findUserById', err)
+  })
+
+  // userModel.checkUser(findUser)
+  //       .then(function(user) {
+  //           if (user) {
+  //               console.log("successful user signin");
+  //               res.status(202).send(user);
+  //           } else {
+  //               res.status(200).send("Please sign up first")
+  //           }
+  //       })
+  //       .catch(function(err) {
+  //           res.status(500).end('Error inside findUserById', err)
+  //       })
 
 }
